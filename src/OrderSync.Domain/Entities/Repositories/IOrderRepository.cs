b205ba1f53549f78c59d47141b1c7697565ac40d
Vitor Santos <vitorspiri1@gmail.com>
2025-07-03 23:44:10 -0300

    feat: order interface and repository

---
+++ src/OrderSync.Domain/Entities/Repositories/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace OrderSync.Domain.Entities.Repositories;
+
+public interface IOrderRepository
+{
+    Task<Order> GetByIdAsync(Ulid id);
+    Task<IEnumerable<Order>> GetAllAsync();
+    Task AddAsync(Order order);
+    Task UpdateAsync(Order order);
+}
---
+++ src/OrderSync.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,27 @@
+﻿using OrderSync.Domain.Entities;
+using OrderSync.Domain.Entities.Repositories;
+
+namespace OrderSync.Infrastructure.Repositories;
+
+public class OrderRepository : IOrderRepository
+{
+    public Task<Order> GetByIdAsync(Ulid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Order>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddAsync(Order order)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Order order)
+    {
+        throw new NotImplementedException();
+    }
+}
