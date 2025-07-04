b37b93783933974556f45d964ad88ff52cd725f7
Vitor Santos <vitorspiri1@gmail.com>
2025-07-03 23:45:35 -0300

    feat: creating MockSapConnector and inheritance in ISAPConnector

---
+++ src/OrderSync.Infrastructure/Connectors/MockSapConnector.cs
@@ -0,0 +1,12 @@
+﻿using OrderSync.Application.Interfaces;
+using OrderSync.Domain.Entities;
+
+namespace OrderSync.Infrastructure.Connectors;
+
+public class MockSapConnector : ISapConnector
+{
+    public Task<List<Order>> GetDelayedOrdersAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
