0064172fc770b7728e141441d5cbdf919ebd3e12
Vitor Santos <vitorspiri1@gmail.com>
2025-07-03 23:43:32 -0300

    feat: database settings and packages

---
+++ src/OrderSync.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderSync.Domain.Entities;
+
+namespace OrderSync.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base (options) { }
+    
+    DbSet<Order> Orders { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
